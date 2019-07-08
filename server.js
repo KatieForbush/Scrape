5dbdf11af13508389ff31e7d5fb58a9b25d1309a
Kate <you@example.com>
2019-07-07 17:03:08 -0700

    YA something on the website

--- server.js
+++ server.js
@@ -17,14 +17,17 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "index"
-  }));
-  app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
-  app.get("/", function (req, res) {
-    res.render("index");
-  });
+//   app.get("/", function (req, res) {
+//     res.render("index", "Article");
+//   });
 
 // Configure middleware
 
@@ -39,7 +42,6 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/scrape", { useNewUrlParser: true });
 
-
 // Routes
 
 // A GET route for scraping the echoJS website
@@ -53,7 +55,7 @@ app.get("/scrape", function(req, res) {
     $("article h2").each(function(i, element) {
       // Save an empty result object
       var result = {};
-//I want to get the title and article from theonion.com
+      //I want to get the title and article from theonion.com
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children()
@@ -61,8 +63,8 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
-        
-        console.log(element);
+
+      console.log(element);
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
@@ -81,12 +83,14 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
+    .populate("Articles")
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      
+      res.render("index", {articles: dbArticle});
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -102,7 +106,7 @@ app.get("/articles/:id", function(req, res) {
     .populate("note")
     .then(function(dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
+      res.json(dbArticle)
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -118,7 +122,11 @@ app.post("/articles/:id", function(req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
